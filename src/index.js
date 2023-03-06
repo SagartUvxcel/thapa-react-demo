7fb1b99ac1d8655c727e9a72e05d8553bb027cf8
SagartUvxcel <sagart@uvxcel.com>
2023-03-06 14:55:23 +0530

    add deployement step

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./index.css"
 import reportWebVitals from "./reportWebVitals";
-import {BrowserRouter as Router} from "react-router-dom"
+import {HashRouter as Router} from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 
